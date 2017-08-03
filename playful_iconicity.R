f0049fe546732bec2c55494cb4a79f9842aae499
mdingemanse <markdingemanse@gmail.com>
2017-08-03 12:22:17 +0200

    some plots

--- playful_iconicity.R
+++ playful_iconicity.R
@@ -14,8 +14,14 @@
 # Clear workspace
 rm(list=ls())
 
+# check for /in/ and /out/ directories (create them if needed)
+add_working_dir <- function(x) { if(file.exists(x)) { cat(x,"dir:",paste0(getwd(),"/",x,"/")) } else { dir.create(paste0(getwd(),"/",x)) 
+  cat("subdirectory",x,"created in",getwd()) } }
+add_working_dir("in")
+add_working_dir("out")
+
 # Packages and useful functions
-list.of.packages <- c("tidyverse","readxl","lme4","ppcor")
+list.of.packages <- c("tidyverse","GGally","readxl","lme4","ppcor")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)>0) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
@@ -45,6 +51,8 @@ df <- merge(iconicity,humor,by="word") %>%
 # Test relation -----------------------------------------------------------
 
 ggpairs(df,columns=c("iconicity","humor","freq_count","KupermanAOA"),lower=list(continuous = "smooth",alpha=0.5))
+ggsave(file="out/playful_iconicity_ggpairs.png",width=6,height=6)
+
 
 # Iconicity much better predictor than frequency, which was the best predictor
 # according to Engelthaler & Hills (2017)
@@ -53,13 +61,16 @@ cor.test(df$humor,df$iconicity)
 
 # Humor shows a positive correlation with iconicity rating, as predicted:
 ggplot(df,aes(iconicity,humor)) +
-  geom_point() +
-  geom_smooth()
+  geom_point(alpha=0.5) +
+  geom_smooth(method="loess")
+ggsave(file="out/playful_iconicity_scatterplot1.png",width=6,height=4)
 
 # The relation is strongest for positive iconicity ratings:
 ggplot(df %>% filter(iconicity > 0),aes(iconicity,humor)) +
-  geom_point() +
-  geom_smooth(method="lm")
+  geom_point(alpha=0.5) +
+  geom_smooth(method="loess")
+ggsave(file="out/playful_iconicity_scatterplot2.png",width=6,height=4)
+
 
 # LME with orthographic length as a random effect shows that a model including 
 # iconicity in addition to frequency provides a significantly better fit.
